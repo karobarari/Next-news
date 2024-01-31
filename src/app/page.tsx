2627a0a301b84d1040bd9647b2667ed3f5cd8a6f
caro <karobarari@gmail.com>
2024-01-31 10:20:19 +0000

    update directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ import Link from "next/link";
 
 export default function Home() {
   return(
-    <Link className="flex justify-center" href={`/prediction/Karo`}>go</Link>
+    <Link className="flex justify-center" href={`/articles`}>go</Link>
   )
 }
